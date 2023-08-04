075ff0f80523accaa6764782a9fae871495c2cc0
soker <sokerimiess@gmail.com>
2023-08-05 04:49:50 +0600

    Initial commit

---
+++ src/prime.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class prime
+{
+    public static boolean isPrime(int n)
+    {
+        if (n <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++)
+        {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isPrime(number))
+        {
+            System.out.println("prime");
+        } else
+        {
+            System.out.println("composite");
+        }
+        scanner.close();
+    }
+}
